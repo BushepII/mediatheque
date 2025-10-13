96def0f8bfd027e2cb6f8382157b7095211b3f3a
BushepII <bushepii@gmail.com>
2025-10-13 12:16:27 +0200

    added login button and a user to login

--- mediatheque_project/mediatheque_project/admin.py
+++ mediatheque_project/mediatheque_project/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from .models import Livre, Dvd, Cd, JeuDePlateau, Emprunteur
+
 
 admin.site.register(Livre)
 admin.site.register(Dvd)
